868e1ceda5c6f91ab8246955ea6eee1413e8ce0f
drlucasmendes <75954959+drlucasmendes@users.noreply.github.com>
2025-02-01 14:56:19 -0500

    Update cloudflare-ddns.py

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -62,7 +62,7 @@ def getIPs():
     if ipv4_enabled:
         try:
             a = requests.get(
-                "https://1.1.1.1/cdn-cgi/trace").text.split("\n")
+                "https://cloudflare.com/cdn-cgi/trace").text.split("\n")
             a.pop()
             a = dict(s.split("=") for s in a)["ip"]
         except Exception:
